c6a83b34eb24d22cf7d14cff620bf7d8937ffb8e
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-08-06 03:48:47 +0700

    03:46

--- database/seeders/PresenceSeeder.php
+++ database/seeders/PresenceSeeder.php
@@ -14,42 +14,48 @@ class PresenceSeeder extends Seeder
      */
     public function run(): void
     {
-        // $sabtu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-05 07:00');
-        // $tempSabtu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-05 07:00');
+        $sabtu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-05 07:00');
+        $tempSabtu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-05 07:00');
 
-        // for ($index = 1; $index <= 19; $index++) {
+        for ($index = 1; $index <= 19; $index++) {
 
-        //     $tempSabtu->addHours(1);
+            sleep(1);
 
-        //     Presence::create([
-        //         'employee_name' => fake()->name(),
-        //         'jabatan' => 'Fake',
-        //         'start_time' => $sabtu->format('Y-m-d H:i'),
-        //         'finish_time' => $tempSabtu->format('Y-m-d H:i'),
-        //         'status' => 'Sabtu',
-        //     ]);
-        // }
+            $tempSabtu->addHours(1);
 
-        // $normal = Carbon::createFromFormat('Y-m-d H:i', '2023-08-07 07:00');
-        // $tempNormal = Carbon::createFromFormat('Y-m-d H:i', '2023-08-07 07:00');
+            Presence::create([
+                'employee_name' => fake()->name(),
+                'jabatan' => 'Fake',
+                'start_time' => $sabtu->format('Y-m-d H:i'),
+                'finish_time' => $tempSabtu->format('Y-m-d H:i'),
+                'status' => 'Sabtu',
+            ]);
+        }
 
-        // for ($index = 1; $index <= 19; $index++) {
+        $normal = Carbon::createFromFormat('Y-m-d H:i', '2023-08-07 07:00');
+        $tempNormal = Carbon::createFromFormat('Y-m-d H:i', '2023-08-07 07:00');
 
-        //     $tempNormal->addHours(1);
+        for ($index = 1; $index <= 19; $index++) {
 
-        //     Presence::create([
-        //         'employee_name' => fake()->name(),
-        //         'jabatan' => 'Fake',
-        //         'start_time' => $normal->format('Y-m-d H:i'),
-        //         'finish_time' => $tempNormal->format('Y-m-d H:i'),
-        //         'status' => 'Normal',
-        //     ]);
-        // }
+            sleep(1);
+
+            $tempNormal->addHours(1);
+
+            Presence::create([
+                'employee_name' => fake()->name(),
+                'jabatan' => 'Fake',
+                'start_time' => $normal->format('Y-m-d H:i'),
+                'finish_time' => $tempNormal->format('Y-m-d H:i'),
+                'status' => 'Normal',
+            ]);
+        }
 
         $minggu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-06 07:00');
         $tempMinggu = Carbon::createFromFormat('Y-m-d H:i', '2023-08-06 07:00');
 
         for ($index = 1; $index <= 19; $index++) {
+
+            sleep(1);
 
             $tempMinggu->addHours(1);
 
@@ -59,7 +65,6 @@ class PresenceSeeder extends Seeder
                 'start_time' => $minggu->format('Y-m-d H:i'),
                 'finish_time' => $tempMinggu->format('Y-m-d H:i'),
                 'status' => 'Libur',
-                'created_at' => $tempMinggu->format('Y-m-d H:i'),
             ]);
         }
     }
