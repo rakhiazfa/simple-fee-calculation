6f58901954832027286bb7d2f1b624980d0a61ff
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-08-06 04:05:23 +0700

    04:05

--- resources/views/presences.blade.php
+++ resources/views/presences.blade.php
@@ -69,7 +69,7 @@
 
         <div class="w-full bg-white p-5">
             <div class="table-responsive">
-                <table class="table table-xs bordered">
+                <table class="table table-xs bordered-b">
                     <thead>
                         <tr>
                             <th class="text-center" rowspan="3">No</th>
